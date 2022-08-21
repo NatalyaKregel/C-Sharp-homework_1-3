c22399b6391e24220deba3cb597d549af159f364
Nata Kregel <natik.kregel@gmai.com>
2022-08-21 15:43:19 +0300

    Добавлена Задача №3 по С#

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+
+{
+    Console.Write("Четное число: ");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.Write("Нечетное число: ");
+    Console.WriteLine(a);
+}
